c96e30df42cee2928b089fa7562309630397b1b4
Nitish Kumar Patel <96331156+nitishpatel404@users.noreply.github.com>
2022-02-12 20:25:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const shoot = () => 
+      {
+<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      }
   return (
     <div id="main">
-      // Do not alter the main div
+      
+     <button onClick={shoot} id="click">Click Here</button>
     </div>
   );
 }
