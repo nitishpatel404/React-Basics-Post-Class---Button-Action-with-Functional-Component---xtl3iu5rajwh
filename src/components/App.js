0571b085ee381a85fd80e6510190ab683a5a3af4
Nitish Kumar Patel <96331156+nitishpatel404@users.noreply.github.com>
2022-02-12 21:58:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,21 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+const[text,usetext]=useState(true)
+  
+  
   const shoot = () => 
-      {
-<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-      }
-  return (
-    <div id="main">
       
-     <button onClick={shoot} id="click">Click Here</button>
+        {usetext(false)
+        }
+  return (
+    
+    <div id="main" >
+         
+          <p id="para" hidden={text} >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+          
+      
+     <button onClick={shoot} id="click" >Click Here</button>
     </div>
   );
 }
