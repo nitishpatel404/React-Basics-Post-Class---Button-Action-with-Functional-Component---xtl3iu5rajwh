670c84fd74a6e313636d68512363fb8245ac5118
Nitish Kumar Patel <96331156+nitishpatel404@users.noreply.github.com>
2022-02-13 08:13:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,16 @@ const[text,usetext]=useState(true)
   
   
   const shoot = () => 
-      
-        {usetext(false)
+     {
+       usetext(false)
         }
   return (
     
     <div id="main" >
+    <div hidden={text}>
          
-          <p id="para" hidden={text} >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+          <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+</div>
           
       
      <button onClick={shoot} id="click" >Click Here</button>
